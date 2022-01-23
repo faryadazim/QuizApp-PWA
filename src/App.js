44d629f7b5280d05417437d7afc95a0e7d3418cf
Faryad Az Zim <73974353+faryadazim@users.noreply.github.com>
2022-01-23 14:44:19 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return <div className="App">Home Page</div>;
+}
+
+export default App;
---
+++ src/User.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const User = () => {
+  return <div className="App">user</div>;
+};
+
+export default User;
