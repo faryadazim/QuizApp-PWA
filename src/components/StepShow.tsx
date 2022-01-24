75311d7ed80906f26185877d5299cf52cac919af
Faryad Az Zim <73974353+faryadazim@users.noreply.github.com>
2022-01-24 15:37:55 +0500

    Update StepShow.tsx

--- src/components/StepShow.tsx
+++ src/components/StepShow.tsx
@@ -1,17 +1,19 @@
 import { Steps } from 'antd';
 import { UserOutlined, SolutionOutlined, LoadingOutlined, SmileOutlined } from '@ant-design/icons';
+import { useState } from 'react';
 
 const { Step } = Steps;
 const StepShow = () => {
+  const [progress, setprogress] = useState(<LoadingOutlined />);
   return <>
-  <div>
-    <Steps>
-      <Step status="process" title="Start" icon={<UserOutlined />} />
-      <Step status="wait" title="Test" icon={<SolutionOutlined />} />
-      <Step status="wait" title="Finished" icon={<LoadingOutlined />} />
-      <Step status="wait" title="Certification" icon={<SmileOutlined />} />
+    <div>
+      <Steps>
+        <Step status="wait" title="Start" icon={progress || UserOutlined} />
+        <Step status="wait" title="Test" icon={<UserOutlined />} />
+        <Step status="wait" title="Finish" icon={< UserOutlined />} />
+        <Step status="wait" title="Certification" icon={<SmileOutlined />} />
 
-    </Steps></div>
+      </Steps></div>
   </>;
 };
 
