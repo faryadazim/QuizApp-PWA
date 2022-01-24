59b500a09af131e6754a25048c6762c5da7296b8
Faryad Az Zim <73974353+faryadazim@users.noreply.github.com>
2022-01-24 19:54:35 +0500

    vv

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 // import { Button } from 'antd';
 import StepShow from './components/StepShow'
@@ -8,76 +7,68 @@ import {
   Form,
   Input,
   Button,
-  Radio,
   Select,
-  Cascader,
-  DatePicker,
   InputNumber,
-  TreeSelect,
-  Switch,
 } from 'antd';
+import QuestionCard from './components/QuestionCard';
 
 const { Option } = Select;
 function App() {
-
+  const [isGameOver, setIsGameOver] = useState(false);
 
   const handleButton = () => {
     console.log("clicked");
+    setIsGameOver(false)
 
   }
   return (
 
-    <>  <div className="bg-image"></div>
-
+    <>
+      <div className="bg-image"></div>
       <div className="App">
         <div className="progress">
           <StepShow />
         </div>
 
 
+
         <div className="form-start">
 
+          {
+            isGameOver ? <div className="formbox">
+              <Form
+                labelCol={{ span: 4 }}
+                wrapperCol={{ span: 14 }}
+                layout="horizontal"
+                className='text-center'
+              >
+                <Form.Item  >
+                  <Input className='tranparent color-light' placeholder='Enter Your Name' />
+                </Form.Item> <Form.Item  >
+                  <InputNumber className='tranparent color-light' placeholder='How Much Question' />
+                </Form.Item>
+                <Form.Item className='tranparent color-light' >
+                  <Select
+                    // showSearch
+                    dropdownStyle={{ backgroundColor: 'grey' }}
+                    placeholder="Select Difficulty Level"
+                    optionFilterProp="children"
+                    className='center'
+                  >
+                    <Option value="easy" className='tranparent'
+                      style={{ backgroundColor: 'green', with: '50px ' }} >Easy</Option>
+                    <Option value="medium" className='tranparent'
+                      style={{ backgroundColor: 'green', with: '50px ' }} >Medium</Option>
+                    <Option value="hard" className='tranparent'
+                      style={{ backgroundColor: 'green', with: '50px ' }} >Hard</Option>
 
+                  </Select>,
+                </Form.Item>
+                <Button type="primary" className='selector' onClick={handleButton} >Start Quiz</Button>
+              </Form>
+            </div> : <QuestionCard />
+          }
 
-          <div className="formbox">
-
-            <Form
-              labelCol={{ span: 4 }}
-              wrapperCol={{ span: 14 }}
-              layout="horizontal"
-              className='text-center'
-            >
-
-              <Form.Item  >
-                <Input className='tranparent color-light' placeholder='Enter Your Name' />
-              </Form.Item> <Form.Item  >
-                <InputNumber className='tranparent color-light' placeholder='How Much Question' />
-              </Form.Item>
-              <Form.Item className='tranparent color-light' >
-                <Select
-                  // showSearch
-                  dropdownStyle={{ backgroundColor: 'grey' }}
-                  placeholder="Select Difficulty Level"
-                  optionFilterProp="children"
-                  className='center'
-                >
-                  <Option value="easy" className='tranparent'
-                    style={{ backgroundColor: 'green', with: '50px ' }} >Easy</Option>
-                  <Option value="medium" className='tranparent'
-                    style={{ backgroundColor: 'green', with: '50px ' }} >Medium</Option>
-                  <Option value="hard" className='tranparent'
-                    style={{ backgroundColor: 'green', with: '50px ' }} >Hard</Option>
-
-                </Select>,
-              </Form.Item>
-
-
-
-
-              <Button type="primary" className='selector' onClick={handleButton} >Start Quiz</Button>
-
-
-            </Form></div>
         </div>
       </div></>
   );
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Input } from 'antd';
+
+const { Search } = Input;
+const { TextArea } = Input;
+
+
+const QuestionCard = () => {
+    return <>
+        <p style={{ marginTop: '12px' }}>
+            Question 2/10
+        </p>
+        <div className='questionBox'>
+            <p className='questionBox1'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic nemo sint harum veritatis rerum.
+            </p>
+        </div>
+
+
+    </>;
+};
+
+export default QuestionCard;
